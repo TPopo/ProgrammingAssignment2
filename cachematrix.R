ee6d5d6e47d0b570d223400c50c881c81a347c14
TPopo <34726001+TPopo@users.noreply.github.com>
2017-12-23 18:03:07 -0500

    Revert "dont know what this does"
    This reverts commit c499b317c99e82011d47c6e9c369ce58075aa56d.

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,15 @@
-## makeCacheMatrix makes a matrix with extra goodies so cacheSolve can
-## look up the inverse, and if found, print it, or otherwise solve it
+## Put comments here that give an overall description of what your
+## functions do
 
-## this makes a matrix and sets up some variables that are able to be located in
-## the parent environment
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-      inv <- NULL
-      set <- function(y) {
-            x <<- y
-            inv <<- NULL
-      }
-      get <- function() x
-      setInverse <- function(inverse) inv <<- inverse
-      getInverse <- function() inv
-      list(set = set,
-           get = get,
-           setInverse = setInverse,
-           getInverse = getInverse)
+
 }
 
-## This looks for the inverse matrix in the parent environment and
-## if it is found, print it, if not it will solve it and print
+
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-      inv <- x$getInverse()
-      if (!is.null(inv)) {
-            message("getting cached data")
-            return(inv)
-      }
-      mat <- x$get()
-      inv <- solve(mat, ...)
-      x$setInverse(inv)
-      return(inv)
+        ## Return a matrix that is the inverse of 'x'
 }
