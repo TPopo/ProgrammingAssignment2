c499b317c99e82011d47c6e9c369ce58075aa56d
TPopo <34726001+TPopo@users.noreply.github.com>
2017-12-23 17:58:37 -0500

    dont know what this does
    dont know what this does

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix makes a matrix with extra goodies so cacheSolve can
+## look up the inverse, and if found, print it, or otherwise solve it
 
-## Write a short comment describing this function
+## this makes a matrix and sets up some variables that are able to be located in
+## the parent environment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      inv <- NULL
+      set <- function(y) {
+            x <<- y
+            inv <<- NULL
+      }
+      get <- function() x
+      setInverse <- function(inverse) inv <<- inverse
+      getInverse <- function() inv
+      list(set = set,
+           get = get,
+           setInverse = setInverse,
+           getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## This looks for the inverse matrix in the parent environment and
+## if it is found, print it, if not it will solve it and print
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      inv <- x$getInverse()
+      if (!is.null(inv)) {
+            message("getting cached data")
+            return(inv)
+      }
+      mat <- x$get()
+      inv <- solve(mat, ...)
+      x$setInverse(inv)
+      return(inv)
 }
